5f9d389fc8aa95c9f2a649919a2548fcf10c4ca7
DanielArmstrng <153695710+DanielArmstrng@users.noreply.github.com>
2025-03-25 09:46:41 +0000

    optimisation, improved performance by A LOT

--- Assets/Scripts/ChunkCube.cs
+++ Assets/Scripts/ChunkCube.cs
@@ -21,97 +21,168 @@ public class ChunkCube
         
     }
 
-    public ChunkCube(Chunk own, Vector3 pos, Blocks blocks)
+    public ChunkCube(Chunk owner, Vector3 position, Blocks blockType, bool[] visibleFaces)
     {
-        owner = own;
-        position = pos;
-        block = blocks;
+        this.owner = owner;
+        this.position = position;
+        this.block = blockType;
 
-        GenerateCube();
+        // Only create the faces that are marked as visible
+        if (visibleFaces[0]) CreateFront(owner, position, blockType);
+        if (visibleFaces[1]) CreateBack(owner, position, blockType);
+        if (visibleFaces[2]) CreateTop(owner, position, blockType);
+        if (visibleFaces[3]) CreateBottom(owner, position, blockType);
+        if (visibleFaces[4]) CreateRight(owner, position, blockType);
+        if (visibleFaces[5]) CreateLeft(owner, position, blockType);
     }
 
-    void GenerateCube()
-    {
-        if(owner.CubeCheck(new Vector3(position.x, position.y, position.z + 1)) == false)  
-            CreateCubeSide("front");
-        if (owner.CubeCheck(new Vector3(position.x, position.y, position.z - 1)) == false)
-            CreateCubeSide("back");
-        if (owner.CubeCheck(new Vector3(position.x, position.y + 1, position.z)) == false)
-            CreateCubeSide("top");
-        if (owner.CubeCheck(new Vector3(position.x, position.y - 1, position.z)) == false)
-            CreateCubeSide("bottom");
-        if (owner.CubeCheck(new Vector3(position.x + 1, position.y, position.z)) == false)
-            CreateCubeSide("left");
-        if (owner.CubeCheck(new Vector3(position.x - 1, position.y, position.z)) == false)
-            CreateCubeSide("right");
-    }
-
-    void CreateCubeSide(string side)
+    void CreateFront(Chunk owner, Vector3 position, Blocks blockType)
     {
         owner.triangles.Add(0 + owner.vertices.Count);
         owner.triangles.Add(1 + owner.vertices.Count);
         owner.triangles.Add(2 + owner.vertices.Count);
-
         owner.triangles.Add(0 + owner.vertices.Count);
         owner.triangles.Add(2 + owner.vertices.Count);
         owner.triangles.Add(3 + owner.vertices.Count);
 
-        //Front side
         Vector3 v0 = new Vector3(0.5f, -0.5f, 0.5f);
         Vector3 v1 = new Vector3(0.5f, 0.5f, 0.5f);
         Vector3 v2 = new Vector3(-0.5f, 0.5f, 0.5f);
         Vector3 v3 = new Vector3(-0.5f, -0.5f, 0.5f);
 
-        //Back side
-        Vector3 v4 = new Vector3(-0.5f, -0.5f, -0.5f);
-        Vector3 v5 = new Vector3(-0.5f, 0.5f, -0.5f);
-        Vector3 v6 = new Vector3(0.5f, 0.5f, -0.5f);
-        Vector3 v7 = new Vector3(0.5f, -0.5f, -0.5f);
+        owner.vertices.Add(v0 + position);
+        owner.vertices.Add(v1 + position);
+        owner.vertices.Add(v2 + position);
+        owner.vertices.Add(v3 + position);
 
+        owner.UVs.Add(new Vector2(1, 0));
         owner.UVs.Add(new Vector2(1, 1));
         owner.UVs.Add(new Vector2(0, 1));
         owner.UVs.Add(new Vector2(0, 0));
-        owner.UVs.Add(new Vector2(1, 0));
-
-        switch (side)
-        {
-            case "front":
-                owner.vertices.Add(v0 + position);
-                owner.vertices.Add(v1 + position);
-                owner.vertices.Add(v2 + position);
-                owner.vertices.Add(v3 + position);
-                break;
-            case "back":
-                owner.vertices.Add(v4 + position);
-                owner.vertices.Add(v5 + position);
-                owner.vertices.Add(v6 + position);
-                owner.vertices.Add(v7 + position);
-                break;
-            case "top":
-                owner.vertices.Add(v1 + position);
-                owner.vertices.Add(v6 + position);
-                owner.vertices.Add(v5 + position);
-                owner.vertices.Add(v2 + position);
-                break;
-            case "bottom":
-                owner.vertices.Add(v3 + position);
-                owner.vertices.Add(v4 + position);
-                owner.vertices.Add(v7 + position);
-                owner.vertices.Add(v0 + position);
-                break;
-            case "left":
-                owner.vertices.Add(v7 + position);
-                owner.vertices.Add(v6 + position);
-                owner.vertices.Add(v1 + position);
-                owner.vertices.Add(v0 + position);
-                break;
-            case "right":
-                owner.vertices.Add(v3 + position);
-                owner.vertices.Add(v2 + position);
-                owner.vertices.Add(v5 + position);
-                owner.vertices.Add(v4 + position);
-                break;
-        }
     }
 
+    void CreateBack(Chunk owner, Vector3 position, Blocks blockType)
+    {
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(1 + owner.vertices.Count);
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(3 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+
+        Vector3 v0 = new Vector3(0.5f, -0.5f, -0.5f);
+        Vector3 v1 = new Vector3(0.5f, 0.5f, -0.5f);
+        Vector3 v2 = new Vector3(-0.5f, 0.5f, -0.5f);
+        Vector3 v3 = new Vector3(-0.5f, -0.5f, -0.5f);
+
+        owner.vertices.Add(v0 + position);
+        owner.vertices.Add(v1 + position);
+        owner.vertices.Add(v2 + position);
+        owner.vertices.Add(v3 + position);
+
+        owner.UVs.Add(new Vector2(0, 0));
+        owner.UVs.Add(new Vector2(0, 1));
+        owner.UVs.Add(new Vector2(1, 1));
+        owner.UVs.Add(new Vector2(1, 0));
+    }
+
+    void CreateTop(Chunk owner, Vector3 position, Blocks blockType)
+    {
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(1 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(3 + owner.vertices.Count);
+
+        Vector3 v0 = new Vector3(0.5f, 0.5f, 0.5f);
+        Vector3 v1 = new Vector3(0.5f, 0.5f, -0.5f);
+        Vector3 v2 = new Vector3(-0.5f, 0.5f, -0.5f);
+        Vector3 v3 = new Vector3(-0.5f, 0.5f, 0.5f);
+
+        owner.vertices.Add(v0 + position);
+        owner.vertices.Add(v1 + position);
+        owner.vertices.Add(v2 + position);
+        owner.vertices.Add(v3 + position);
+
+        owner.UVs.Add(new Vector2(1, 1));
+        owner.UVs.Add(new Vector2(1, 0));
+        owner.UVs.Add(new Vector2(0, 0));
+        owner.UVs.Add(new Vector2(0, 1));
+    }
+
+    void CreateBottom(Chunk owner, Vector3 position, Blocks blockType)
+    {
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(1 + owner.vertices.Count);
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(3 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+
+        Vector3 v0 = new Vector3(0.5f, -0.5f, 0.5f);
+        Vector3 v1 = new Vector3(0.5f, -0.5f, -0.5f);
+        Vector3 v2 = new Vector3(-0.5f, -0.5f, -0.5f);
+        Vector3 v3 = new Vector3(-0.5f, -0.5f, 0.5f);
+
+        owner.vertices.Add(v0 + position);
+        owner.vertices.Add(v1 + position);
+        owner.vertices.Add(v2 + position);
+        owner.vertices.Add(v3 + position);
+
+        owner.UVs.Add(new Vector2(1, 1));
+        owner.UVs.Add(new Vector2(1, 0));
+        owner.UVs.Add(new Vector2(0, 0));
+        owner.UVs.Add(new Vector2(0, 1));
+    }
+
+    void CreateRight(Chunk owner, Vector3 position, Blocks blockType)
+    {
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(1 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(3 + owner.vertices.Count);
+
+        Vector3 v0 = new Vector3(0.5f, -0.5f, -0.5f);
+        Vector3 v1 = new Vector3(0.5f, 0.5f, -0.5f);
+        Vector3 v2 = new Vector3(0.5f, 0.5f, 0.5f);
+        Vector3 v3 = new Vector3(0.5f, -0.5f, 0.5f);
+
+        owner.vertices.Add(v0 + position);
+        owner.vertices.Add(v1 + position);
+        owner.vertices.Add(v2 + position);
+        owner.vertices.Add(v3 + position);
+
+        owner.UVs.Add(new Vector2(1, 0));
+        owner.UVs.Add(new Vector2(1, 1));
+        owner.UVs.Add(new Vector2(0, 1));
+        owner.UVs.Add(new Vector2(0, 0));
+    }
+
+    void CreateLeft(Chunk owner, Vector3 position, Blocks blockType)
+    {
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+        owner.triangles.Add(1 + owner.vertices.Count);
+        owner.triangles.Add(0 + owner.vertices.Count);
+        owner.triangles.Add(3 + owner.vertices.Count);
+        owner.triangles.Add(2 + owner.vertices.Count);
+
+        Vector3 v0 = new Vector3(-0.5f, -0.5f, -0.5f);
+        Vector3 v1 = new Vector3(-0.5f, 0.5f, -0.5f);
+        Vector3 v2 = new Vector3(-0.5f, 0.5f, 0.5f);
+        Vector3 v3 = new Vector3(-0.5f, -0.5f, 0.5f);
+
+        owner.vertices.Add(v0 + position);
+        owner.vertices.Add(v1 + position);
+        owner.vertices.Add(v2 + position);
+        owner.vertices.Add(v3 + position);
+
+        owner.UVs.Add(new Vector2(0, 0));
+        owner.UVs.Add(new Vector2(0, 1));
+        owner.UVs.Add(new Vector2(1, 1));
+        owner.UVs.Add(new Vector2(1, 0));
+    }
 }
--- Assets/Scripts/Chunks.cs
+++ Assets/Scripts/Chunks.cs
@@ -126,14 +126,44 @@ public class Chunk
                 {
                     if (chunkMap[x, y, z] != BlockDB.GetBlockName("Air"))
                     {
-                        if (!IsFullySurrounded(new Vector3(x, y, z)))
+                        Vector3 pos = new Vector3(x, y, z);
+                        bool[] visibleFaces = GetVisibleFaces(pos);
+                        if (HasAnyVisibleFace(visibleFaces))
                         {
-                            ChunkCube newCube = new ChunkCube(this, new Vector3(x, y, z), chunkMap[x, y, z]);
+                            ChunkCube newCube = new ChunkCube(this, pos, chunkMap[x, y, z], visibleFaces);
                         }
                     }
                 }
 
         GeneratePhysicalChunk();
+    }
+
+    private bool[] GetVisibleFaces(Vector3 pos)
+    {
+        // Order: front, back, top, bottom, right, left
+        bool[] visibleFaces = new bool[6];
+        int x = (int)pos.x;
+        int y = (int)pos.y;
+        int z = (int)pos.z;
+
+        // Check each face
+        visibleFaces[0] = !CubeCheck(new Vector3(x, y, z + 1)); // front
+        visibleFaces[1] = !CubeCheck(new Vector3(x, y, z - 1)); // back
+        visibleFaces[2] = !CubeCheck(new Vector3(x, y + 1, z)); // top
+        visibleFaces[3] = !CubeCheck(new Vector3(x, y - 1, z)); // bottom
+        visibleFaces[4] = !CubeCheck(new Vector3(x + 1, y, z)); // right
+        visibleFaces[5] = !CubeCheck(new Vector3(x - 1, y, z)); // left
+
+        return visibleFaces;
+    }
+
+    private bool HasAnyVisibleFace(bool[] visibleFaces)
+    {
+        for (int i = 0; i < visibleFaces.Length; i++)
+        {
+            if (visibleFaces[i]) return true;
+        }
+        return false;
     }
 
     public bool CubeCheck(Vector3 position)
@@ -144,41 +174,105 @@ public class Chunk
 
         if (x < 0 || x >= World.chunkSize || y < 0 || y >= World.chunkSize || z < 0 || z >= World.chunkSize)
         {
-            Chunk neighbourChunk = World.GetChunk(chunkObject.transform.position + position);
+            Vector3 neighborPos = chunkObject.transform.position;
+            Vector3 checkPos = position;
 
-            x = ConvertIndexToLocal(x);
-            y = ConvertIndexToLocal(y);
-            z = ConvertIndexToLocal(z);
+            // Adjust the position to check the correct neighboring chunk
+            if (x < 0)
+            {
+                neighborPos.x -= World.chunkSize;
+                checkPos.x = World.chunkSize - 1;
+            }
+            else if (x >= World.chunkSize)
+            {
+                neighborPos.x += World.chunkSize;
+                checkPos.x = 0;
+            }
+
+            if (y < 0)
+            {
+                neighborPos.y -= World.chunkSize;
+                checkPos.y = World.chunkSize - 1;
+            }
+            else if (y >= World.chunkSize)
+            {
+                neighborPos.y += World.chunkSize;
+                checkPos.y = 0;
+            }
+
+            if (z < 0)
+            {
+                neighborPos.z -= World.chunkSize;
+                checkPos.z = World.chunkSize - 1;
+            }
+            else if (z >= World.chunkSize)
+            {
+                neighborPos.z += World.chunkSize;
+                checkPos.z = 0;
+            }
+
+            Chunk neighbourChunk = World.GetChunk(neighborPos);
 
             if (neighbourChunk != null)
-                return neighbourChunk.CubeCheck(new Vector3(x, y, z));
+            {
+                return neighbourChunk.chunkMap[(int)checkPos.x, (int)checkPos.y, (int)checkPos.z] == BlockDB.GetBlockName("Dirt");
+            }
             else
-                return false;
+            {
+                // If we're checking above the chunk, treat it as air
+                if (y >= World.chunkSize)
+                {
+                    return false;
+                }
+                // For other missing neighbors, treat as solid
+                return true;
+            }
         }
-
-        else if (chunkMap[x, y, z].isTransparent)
-            return false;
-
-        else             
-            return true;
+        else
+        {
+            return chunkMap[x, y, z] == BlockDB.GetBlockName("Dirt");
+        }
     }
 
-    private bool IsFullySurrounded(Vector3 pos)
-    {
-        return CubeCheck(new Vector3(pos.x, pos.y, pos.z + 1)) &&
-               CubeCheck(new Vector3(pos.x, pos.y, pos.z - 1)) &&
-               CubeCheck(new Vector3(pos.x, pos.y + 1, pos.z)) &&
-               CubeCheck(new Vector3(pos.x, pos.y - 1, pos.z)) &&
-               CubeCheck(new Vector3(pos.x + 1, pos.y, pos.z)) &&
-               CubeCheck(new Vector3(pos.x - 1, pos.y, pos.z));
-    }
+    //private bool IsFullySurrounded(Vector3 pos)
+    //{
+    //    // Only check for surrounding blocks if we're not at a chunk boundary
+    //    bool isEdgeBlock = pos.x == 0 || pos.x == World.chunkSize - 1 ||
+    //                      pos.y == 0 || pos.y == World.chunkSize - 1 ||
+    //                      pos.z == 0 || pos.z == World.chunkSize - 1;
 
-    int ConvertIndexToLocal(int i)
-    {
-        if (i == -1)
-            i = World.chunkSize - 1;
-        else if (i == World.chunkSize)
-            i = 0;
-        return i;
-    }
+    //    if (isEdgeBlock)
+    //    {
+    //        // For edge blocks, we need to check if there's actually a neighboring chunk
+    //        return CubeCheck(new Vector3(pos.x, pos.y, pos.z + 1)) &&
+    //               CubeCheck(new Vector3(pos.x, pos.y, pos.z - 1)) &&
+    //               CubeCheck(new Vector3(pos.x, pos.y + 1, pos.z)) &&
+    //               CubeCheck(new Vector3(pos.x, pos.y - 1, pos.z)) &&
+    //               CubeCheck(new Vector3(pos.x + 1, pos.y, pos.z)) &&
+    //               CubeCheck(new Vector3(pos.x - 1, pos.y, pos.z));
+    //    }
+    //    else
+    //    {
+    //        // For internal blocks, we can use direct array access which is faster
+    //        int x = (int)pos.x;
+    //        int y = (int)pos.y;
+    //        int z = (int)pos.z;
+            
+    //        return !chunkMap[x, y, z + 1].isTransparent &&
+    //               !chunkMap[x, y, z - 1].isTransparent &&
+    //               !chunkMap[x, y + 1, z].isTransparent &&
+    //               !chunkMap[x, y - 1, z].isTransparent &&
+    //               !chunkMap[x + 1, y, z].isTransparent &&
+    //               !chunkMap[x - 1, y, z].isTransparent;
+    //    }
+    //}
+
+    //int ConvertIndexToLocal(int i)
+    //{
+    //    if (i == -1)
+    //        i = World.chunkSize - 1;
+    //    else if (i == World.chunkSize)
+    //        i = 0;
+    //    return i;
+    //}
 }
